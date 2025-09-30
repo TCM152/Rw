28f8050012df158a9d5b1655537d4c93db65d14c
TCM152 <github.1lol@outlook.com>
2025-09-30 07:39:00 +0700

    Create hi.js

---
+++ hi.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+          apiKey: "AIzaSyDDImo9Vq1jqID6WDbGMXToexeIfNFOjpQ",
+          authDomain: "iplo-50a21.firebaseapp.com",
+          databaseURL: "https://iplo-50a21-default-rtdb.asia-southeast1.firebasedatabase.app",
+          projectId: "iplo-50a21",
+          storageBucket: "iplo-50a21.firebasestorage.app",
+          messagingSenderId: "488527718585",
+          appId: "1:488527718585:web:f1a35e256a0d3cf5021368",
+          measurementId: "G-1LQMBTQBPK"
+};
